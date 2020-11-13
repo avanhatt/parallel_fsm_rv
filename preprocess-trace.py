956855fb5919aca199781073404c12ca9cafb164
Alexa VanHattum <amv94@cornell.edu>
2020-11-13 11:25:17 -0500

    More preprocessing options

--- preprocess-trace.py
+++ preprocess-trace.py
@@ -1,21 +1,34 @@
 import click
 from collections import defaultdict
+import os
 import re
 import sys
 
-def preprocess():
+@click.command()
+@click.argument('out',
+    type=click.Path(),
+    default='slices',
+    metavar='<out>')
+@click.option('--dedup/--no-dedup', default=True)
+def preprocess(out, dedup):
     slicing = defaultdict(list)
     for line in sys.stdin.readlines():
         line = line.replace("AVH:", "")
         [event, instance, _] = re.split('(\d.*)', line)
         slicing[instance].append(event)
+
+    # Output dir
+    if not os.path.exists(out):
+        os.makedirs(out)
+
     seen_traces = set()
     for instance, events in slicing.items():
-        if "{}".format(events) in seen_traces:
-            print("Skipping duplicate trace")
+        if dedup and "{}".format(events) in seen_traces:
+            print("Skipping duplicate trace:", instance)
             continue
         seen_traces.add("{}".format(events))
-        with open("slice_{}".format(instance), "w") as file:
+        slice_path = os.path.join(out, instance)
+        with open(slice_path, "w") as file:
             for event in events:
                 file.write("{}\n".format(event))
 
