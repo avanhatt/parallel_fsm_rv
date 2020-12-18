1ea0e7053d18af23e69c7625b416b5e85cf7101b
Alexa VanHattum <amv94@cornell.edu>
2020-12-17 22:19:24 -0500

    More eval

--- offline-monitor-traces.py
+++ offline-monitor-traces.py
@@ -4,15 +4,38 @@ import re
 import sys
 import subprocess
 
+executable = "./target/release/parallel_fsm_rv"
+
+
+def num_events(filename):
+    with open(filename) as f:
+        return len([0 for _ in f])
+
 @click.command()
 @click.argument('slices',
     type=click.Path(),
     default='slices',
     metavar='<out>')
-def monitor(slices):
+@click.option('-p', '--par', type=str)
+@click.option('-s', '--size', type=int)
+def monitor(slices, par, size):
+    # Build the executable
+    subprocess.check_call(["cargo", "build"])
+
+    # Parse options
+    flags = []
+    if par:
+        if int(par) == 1:
+            flags += ["--seq"]
+        else:
+            flags += ["--par", par]
+
     for filename in os.listdir(slices):
+        if size:
+            if num_events(os.path.join(slices, filename)) < size:
+                continue
         print("Checking trace:", filename)
-        subprocess.run(["cargo", "run", "--", os.path.join(slices, filename)])
+        subprocess.run([executable, os.path.join(slices, filename)] + flags)
 
 if __name__ == '__main__':
     monitor()
